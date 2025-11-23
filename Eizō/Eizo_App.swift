be600d442cae5987b008e4eda57602cace7f2648
Hugo Lageneste <hugo.lageneste@pm.me>
2025-11-23 16:14:20 +0100

    Initial Commit

---
+++ Eizō/Eizo_App.swift
@@ -0,0 +1,17 @@
+//
+//  Eizo_App.swift
+//  Eizō
+//
+//  Created by Hugo on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Eizo_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EizōTests/Eizo_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Eizo_Tests.swift
+//  EizōTests
+//
+//  Created by Hugo on 23.11.2025.
+//
+
+import Testing
+@testable import Eizo_
+
+struct Eizo_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
